e4e114d82eae1d50ddaaa449bb2494667af4539c
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-05-31 14:59:43 +0200

    [lisp] Add Emacs .dir-locals.el snippet

---
+++ lisp/emacs-dir-locals/.dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . (
+         ; Set a variable.
+         (eval . (setq my/example-variable "example value"))
+         ; Define a function.
+         (eval . (defun my/example-function (example-argument)
+                   (print example-argument)))
+         )))
