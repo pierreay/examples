c6cf59bb9fd1d01ee4547860d1f9d9ef4cc45fa6
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-06-28 18:37:48 +0200

    [python/scipy_filters] Start filters example

---
+++ python/scipy_filters.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import signal
+from scipy.signal import butter, lfilter
+
+def normalize(arr):
+    """Apply min-max feature scaling normalization to a 1D array."""
+    return (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+
+def butter_bandstop(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype="bandstop")
+    return b, a
+
+def butter_bandstop_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandstop(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype="band")
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype="low", analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def butter_highpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype="high", analog=False)
+    return b, a
+
+def butter_highpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_highpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def plot_time_freq():
+    pass
+
+if __name__ == "__main__":
+    # Generate a composed signal with Numpy to operate on it.
+    pass
