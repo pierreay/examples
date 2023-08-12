14421eef6b9c90c7237a175a5b30b119653e013f
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-08-12 21:46:10 +0200

    [bash] more user choice

--- bash/user_choice.sh
+++ bash/user_choice.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# * "read" Y/N
+
+while true; do
+    read -p "Do you wish to install this program? [y/n] " yn
+    case $yn in
+        [Yy]* ) echo "Installation"; break;;
+        [Nn]* ) echo "Does not install"; break;;
+        * ) echo "Please, answer yes or no.";;
+    esac
+done
+
+# * "select" Menu
+
 PS3='Please, enter your choice and press [ENTER]: '
 opts=("Option 1" "Option 2" "Option 3" "Quit")
 select opt in "${opts[@]}"
@@ -18,5 +31,15 @@ do
             break
             ;;
         *) echo "Invalid option: $REPLY";;
+    esac
+done
+
+# * "select" Y/N
+
+echo "Do you wish to install this program?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "Installation"; break;;
+        No ) echo "Does not install"; break;;
     esac
 done
