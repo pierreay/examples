55ddd8c5407595440616f2dedf177cfa6a18048c
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-10-23 13:09:31 +0200

    [bash] arguments_getopt

--- bash/arguments_getopt.sh
+++ bash/arguments_getopt.sh
@@ -7,9 +7,12 @@
 # Reset in case getopts has been used previously in the shell.
 OPTIND=1
 
-# Program's variables.
-output_file=""
+# Program's options (switches).
 verbose=0
+# Program's option-arguments (variables).
+output_file=""
+# Program's arguments.
+args=""
 
 # Program's help.
 function help() {
@@ -24,20 +27,27 @@ if [[ $# -lt 1 ]]; then
     help
 fi
 
+# Iterate over all supplied options and option-arguments.
+# "f:" means that -f is an option-argument.
+# "?" means that -? is an alias for -h.
 while getopts "h?vf:" opt; do
   case "$opt" in
     h|\?)
       help
       ;;
-    v)  verbose=1
+    v) verbose=1
       ;;
-    f)  output_file=$OPTARG
+    f) output_file=$OPTARG
       ;;
   esac
 done
 
+# Get rid of all arguments processed by getopts.
 shift $((OPTIND-1))
-
+# Skip the "--" keyword indicating that the following is a custom input.
 [ "${1:-}" = "--" ] && shift
+# Get the ARGS.
+args="$@"
 
-echo "verbose=$verbose, output_file='$output_file', Leftovers: $@"
+# Use the options and option-arguments in some way.
+echo "verbose=$verbose, output_file='$output_file', Leftovers: $args"
