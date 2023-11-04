10db87c56fc14baacab2b5cc4c790c1280a89c3b
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-11-04 21:54:31 +0100

    [bash] add dialog example

---
+++ bash/dialog.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# * About
+
+# Demonstrate how the dialog program can be used to create menus.
+# Dialog should be installed using a package manager, for example:
+# sudo pacman -S dialog
+
+# * Dialog demonstration
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Backtitle here"
+TITLE="Title here"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "Option 1"
+         2 "Option 2"
+         3 "Option 3")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "You chose Option 1"
+            ;;
+        2)
+            echo "You chose Option 2"
+            ;;
+        3)
+            echo "You chose Option 3"
+            ;;
+esac
